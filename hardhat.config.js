fa2459ad6ce53071f7ea12daf2ec97a51302d282
Muhammad Sibghat Ullah <59176632+MuhammadSibghatUllah@users.noreply.github.com>
2026-06-03 17:13:30 +0500

    MultiSwap cross-chain bridge architecture

---
+++ hardhat.config.js
@@ -0,0 +1,41 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+const { ethers } = require("ethers");
+require('dotenv').config()
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.2",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    goerli: {
+      url: `https://nd-018-780-500.p2pify.com/8d55fdf55750fe8f435ef82b610d1bba`,
+      accounts:[process.env.PRIVATE_KEY0, process.env.PRIVATE_KEY1] 
+    },
+    ethereum: {
+      url: `https://nd-611-696-948.p2pify.com/8a54d0bc389e645253087fd1a6c5fe3a`,
+      accounts:[process.env.PRIVATE_KEY0, process.env.PRIVATE_KEY1] 
+    },
+    binance: {
+      url: `https://nd-409-138-440.p2pify.com/a2b2f87cd496703b1cc64ff8e91b7981`,
+      accounts:[process.env.PRIVATE_KEY0, process.env.PRIVATE_KEY1] 
+    },
+  },
+  etherscan: {
+    apiKey: process.env.BSCSCAN_API_KEY,
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+  },
+};
